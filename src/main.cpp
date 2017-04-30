9fc766d4f7539fa88000334d23bb1dbbb87714e5
pkern90 <pkern90@gmail.com>
2017-05-01 00:18:23 +0200

    Increase throttle to make car moving

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ int main()
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = 0.5;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
