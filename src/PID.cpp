415135cd7ef9c19568c2ea6f8d929afb3de82464
pkern90 <pkern90@gmail.com>
2017-05-01 13:37:19 +0200

    feat: basic pid impl

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,6 +1,6 @@
 #include "PID.h"
 
-using namespace std;
+//using namespace std;
 
 /*
 * TODO: Complete the PID class.
@@ -10,12 +10,19 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double kp, double ki, double kd) {
+    Kp = kp;
+    Ki = ki;
+    Kd = kd;
 }
 
 void PID::UpdateError(double cte) {
+    d_error = cte - p_error;
+    p_error = cte;
+    i_error = i_error + cte;
 }
 
 double PID::TotalError() {
+    return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
